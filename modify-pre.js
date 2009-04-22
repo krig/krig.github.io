8bc0700e8f901baf8d069374a2081b40dd212daf
Kristoffer Grönlund <kristoffer.gronlund@purplescout.se>
2009-04-22 22:14:42 +0200

    Revised look, some new content

--- modify-pre.js
+++ modify-pre.js
@@ -1,10 +1,13 @@
 function modifyPre() {
     var pres = document.getElementsByTagName('pre');
-    for (var i = 0; i < pres.length; i++) {
+
+    var i;
+    for (i = 0; i < pres.length; i++) {
 	pres[i].className = 'prettyprint';
     }
+
     var codes = document.getElementsByTagName('code');
-    for (var i = 0; i < codes.length; i++) {
+    for (i = 0; i < codes.length; i++) {
 	codes[i].className = 'prettyprint';
     }
 }
